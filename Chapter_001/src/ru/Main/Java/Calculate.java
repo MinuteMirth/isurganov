69a6ca6888daa51a02ca1de517cf07a95f168b89
MinuteMirth <fuzzymuzzy@gmail.com>
2017-06-25 20:16:21 +0300

    JavaDoc.Code convention

--- Chapter_001/src/ru/Main/Java/Calculate.java
+++ Chapter_001/src/ru/Main/Java/Calculate.java
@@ -1,5 +1,16 @@
 package ru.job4j;
+/**
+ * Calculate.
+ *
+ * @author Ivan Surganov(fuzzymuzzy32@gmail.com)
+ * @version 1.0.0
+ * @since 24.06.2017
+ */
 public class Calculate {
+  /**
+    * Main.
+    * @param args - args.
+    */
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
