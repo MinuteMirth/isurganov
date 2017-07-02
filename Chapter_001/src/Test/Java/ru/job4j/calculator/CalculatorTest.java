73abfa137a6a0fb739f9db4658962d81934d32f4
MinuteMirth <fuzzymuzzy@gmail.com>
2017-07-02 16:19:20 +0300

    CalculatorTest

--- Chapter_001/src/Test/Java/ru/job4j/calculator/CalculatorTest.java
+++ Chapter_001/src/Test/Java/ru/job4j/calculator/CalculatorTest.java
@@ -1,4 +1,11 @@
 package ru.job4j.calculator;
+/**
+ * Test.
+ *
+ * @author Ivan Surganov (fuzzymuzzy32@gmail.com)
+ * @version 1.0.0
+ * @since 1.07.2017
+ */
 
 import org.junit.Test;
 
@@ -6,6 +13,9 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class CalculatorTest {
+  /**
+   * Test add.
+   */
     @Test
     public void whenAddOnePlusOneThenTwo() {
         Calculator calc = new Calculator();
@@ -15,23 +25,24 @@ public class CalculatorTest {
         assertThat(result, is(expected));
         }
     public void whenAddOneMinusOneThenZero() {
-      Calculator calc=new Calculator();
+      Calculator calc = new Calculator();
       calc.add(1D,1D);
-      double result=calc.getResult();
-      double expected=0D;
+      double result = calc.getResult();
+      double expected = 0D;
       assertThat(result,is(expected));
         }
     public void whenAddOneMultipliedByOneThenOne() {
-       Calculator calc=new Calculator() ;
+       Calculator calc = new Calculator();
        calc.add(1D,1D);
-       double result=calc.getResult();
-       double expected=1D
+       double resul t = calc.getResult();
+       double expected = 1D;
        asserThat(result,is(expected));
         }
     public void whenAddOneDividedByOneThenOne() {
-           Calculator calc=new Calculator() ;
+           Calculator calc = new Calculator();
            calc.add(1D,1D);
-           double result=calc.getResult();
-           double expected=1D
+           double result = calc.getResult();
+           double expected = 1D;
+           asserThat(result,is(expected));
        }
 }
